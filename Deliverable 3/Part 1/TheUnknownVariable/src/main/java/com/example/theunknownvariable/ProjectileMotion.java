9b12390f5436bb339148ce88687e740ad5dcee88
Meghu-design <meghry.habib@gmail.com>
2025-03-17 18:37:12 -0400

    added projectile motion

---
+++ Deliverable 3/Part 1/TheUnknownVariable/src/main/java/com/example/theunknownvariable/ProjectileMotion.java
@@ -0,0 +1,21 @@
+package com.example.demo1;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ProjectileMotion extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) {
+        UserInterface view = new UserInterface(); // Use the StackPane-based view
+        Scene scene = new Scene(view, 1366, 768);
+        primaryStage.setTitle("Kinematics Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
